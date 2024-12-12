b3921e4a9e6740ad1edf8e7613e2db50eaa2f15b
rawdaraafat <144979504+rawdaraafat@users.noreply.github.com>
2024-12-12 14:56:15 +0200

    Update mainwindow.h

--- TicTacToeGames/mainwindow.h
+++ TicTacToeGames/mainwindow.h
@@ -3,26 +3,28 @@
 
 #include <QMainWindow>
 #include "newwindow.h"
+#include "nplayer.h"
 
-QT_BEGIN_NAMESPACE
 namespace Ui {
 class MainWindow;
 }
-QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
+    explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
 private slots:
     void on_pushButton_clicked();
+    void handleChoiceMade(); // Slot to handle when the user makes a choice
 
 private:
     Ui::MainWindow *ui;
     newwindow *newWindow;
+    nPlayer *nplayer;
 };
+
 #endif // MAINWINDOW_H
