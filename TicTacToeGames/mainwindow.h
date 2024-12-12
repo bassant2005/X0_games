8db5b415dc7ac05328d2a7b68d5666103ea5c1a2
rawdaraafat <144979504+rawdaraafat@users.noreply.github.com>
2024-12-12 10:26:56 +0200

    Update mainwindow.h

--- TicTacToeGames/mainwindow.h
+++ TicTacToeGames/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "newwindow.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -22,5 +23,6 @@ private slots:
 
 private:
     Ui::MainWindow *ui;
+    newwindow *newWindow;
 };
 #endif // MAINWINDOW_H
