5aadcbc06e646e14ca3d144b2b322c055863076f
rawdaraafat <144979504+rawdaraafat@users.noreply.github.com>
2024-12-12 14:57:36 +0200

    Update newwindow.cpp

--- TicTacToeGames/newwindow.cpp
+++ TicTacToeGames/newwindow.cpp
@@ -1,9 +1,12 @@
 #include "newwindow.h"
 #include "ui_newwindow.h"
+#include "nplayer.h"
 
 newwindow::newwindow(QWidget *parent)
     : QDialog(parent)
     , ui(new Ui::newwindow)
+    , susgame(nullptr)
+    , nplayer(nullptr)
 {
     ui->setupUi(this);
 }
@@ -11,6 +14,8 @@ newwindow::newwindow(QWidget *parent)
 newwindow::~newwindow()
 {
     delete ui;
+    if (susgame) delete susgame;
+    if (nplayer) delete nplayer;
 }
 
 void newwindow::on_pushButton_clicked()
@@ -23,5 +28,18 @@ void newwindow::on_pushButton_clicked()
     if (parentWindow) {
         parentWindow->show();
     }
+}
+
+
+
+void newwindow::on_pushButton_2_clicked()
+{
+
+    if(!susgame){
+        susgame = new SUSGame(this);
+    }
+    this->hide();
+    susgame->show();
+
 }
 
